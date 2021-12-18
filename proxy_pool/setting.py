90d3a554244423bb1f9015efbe33d128e636e5bc
mogita <me@mogita.com>
2021-12-18 16:25:54 +0800

    feat: adjust proxy verification conditions

--- proxy_pool/setting.py
+++ proxy_pool/setting.py
@@ -55,10 +55,10 @@ HTTP_URL = "http://httpbin.org"
 HTTPS_URL = "https://www.qq.com"
 
 # 代理验证时超时时间
-VERIFY_TIMEOUT = 10
+VERIFY_TIMEOUT = 3
 
 # 近PROXY_CHECK_COUNT次校验中允许的最大失败次数,超过则剔除代理
-MAX_FAIL_COUNT = 0
+MAX_FAIL_COUNT = 3
 
 # 近PROXY_CHECK_COUNT次校验中允许的最大失败率,超过则剔除代理
 # MAX_FAIL_RATE = 0.1
