1467ac52f34626515a33d6d30aa3ce52c05e47a4
mogita <me@mogita.com>
2021-12-18 22:15:21 +0800

    feat: short-circuit on none proxy results

--- src/http.py
+++ src/http.py
@@ -1,8 +1,11 @@
 import requests
 import time
 import numpy as np
+import logging
 from src.ua import get_a_random_ua
 from src.proxy_pool import get_proxy, delete_proxy
+
+log = logging.getLogger(__name__)
 
 rejected_proxies = {}
 MAX_REJECTED = 5
@@ -12,7 +15,11 @@ def req(url):
     proxy = next_proxy()
     source = None
 
-    while retry_count > 0:
+    if proxy == None:
+        log.error(f"proxy is none while fetching {url}")
+
+    while retry_count > 0 and proxy != None:
+        time.sleep(np.random.rand()*5)
         try:
             proxies = {"http": f"http://{proxy}"}
             resp = requests.get(url, headers={'User-Agent': get_a_random_ua()}, proxies=proxies)
@@ -20,7 +27,6 @@ def req(url):
             break
         except requests.exceptions.RequestException as err:
             retry_count -= 1
-            time.sleep(np.random.rand()*5)
     if retry_count == 0:
         reject_proxy(proxy)
 
