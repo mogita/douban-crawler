9c9a19a9bd61a7bada775d49eadf92c752600c9d
mogita <me@mogita.com>
2022-01-03 19:42:17 +0800

    feat: extract before use

--- migrate.sh
+++ migrate.sh
@@ -2,5 +2,5 @@
 
 echo "running migration..."
 
-migrate -database "postgres://crawler:crawler@douban-crawler-db:5432/crawler?sslmode=disable&search_path=public" -path /db/migrations up
+/usr/local/bin/migrate -database "postgres://crawler:crawler@douban-crawler-db:5432/crawler?sslmode=disable&search_path=public" -path /db/migrations up
 
