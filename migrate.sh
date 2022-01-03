3c99515124696a202d07d2fcfce42e79d7865102
mogita <me@mogita.com>
2022-01-03 18:10:43 +0800

    fix: add execute env and logging

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+
+echo "running migration..."
+
 migrate -database "postgres://crawler:crawler@douban-crawler-db:5432/crawler?sslmode=disable&search_path=public" -path /db/migrations up
 
